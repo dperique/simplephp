0ad85d6c0dee1e7a432a881d03664a2f2bd0a040
Dennis Periquet <Dennis.Periquet@ibm.com>
2021-11-14 14:08:21 -0500

    mention redhat

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 
-echo "This simple web application on Openshift 3.11";
+echo "This on redhat.com simple web application on Openshift 3.11";
 echo "<br>";
 echo "This simple web application on Openshift 3.11";
 echo "This is simple web application on Openshift 3.11";
