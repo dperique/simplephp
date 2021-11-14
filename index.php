47297815dfd0da19a12a0387ddea7d2ec0d2d2ac
Dennis Periquet <Dennis.Periquet@ibm.com>
2021-11-14 10:49:18 -0500

    space5

--- index.php
+++ index.php
@@ -2,6 +2,6 @@
 
 echo "This simple web application on Openshift 3.11";
 echo "<br>";
-echo "This is simple web application on Openshift 3.11";
+echo "This simple web application on Openshift 3.11";
 echo "This is simple web application on Openshift 3.11";
 
