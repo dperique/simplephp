eeef225e66ae85e39087fd8944c8af2cae2a3d87
Dennis Periquet <dperique@gmail.com>
2021-11-15 08:42:28 -0500

    mention 4.9 and use breaks

--- index.php
+++ index.php
@@ -1,7 +1,8 @@
 <?php
 
-echo "This on redhat.com simple web application on Openshift 3.11";
+echo "This on redhat.com simple web application on Openshift 4.9";
 echo "<br>";
-echo "This simple web application on Openshift 3.11";
-echo "This is simple web application on Openshift 3.11";
+echo "This simple web application on Openshift 4.9";
+echo "<br>";
+echo "This is simple web application on Openshift 4.9";
 
